7c359fb43fef85b876a7fbdae9cca128a41e528d
akshaya.biswal <akshaya.join@google.com>
2023-10-01 15:42:01 +0530

    About

--- components/About.tsx
+++ components/About.tsx
@@ -17,27 +17,22 @@ export default function About() {
         After graduating with a degree in{" "}
         <span className="font-medium">Computer Science</span>, I decided to
         pursue my passion for programming. I enrolled in a coding bootcamp and
-        learned <span className="font-medium">full-stack web development</span>.{" "}
+        learned <span className="font-medium">front-end web development</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
-        problem-solving aspect. I <span className="underline">love</span> the
-        feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
-        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
-        learn new technologies. I am currently looking for a{" "}
+        building things. I <span className="underline">love</span> the feeling
+        of finally figuring out a solution to a problem. My core stack is{" "}
+        <span className="font-medium">React, Next.js</span>. I am also familiar
+        with TypeScript. I am always looking to learn new technologies. I am
+        currently looking for a{" "}
         <span className="font-medium">full-time position</span> as a software
         developer.
       </p>
 
       <p>
         <span className="italic">When I&apos;m not coding</span>, I enjoy
-        playing video games, watching movies, and playing with my dog. I also
-        enjoy <span className="font-medium">learning new things</span>. I am
-        currently learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I&apos;m
-        also learning how to play the guitar.
+        travelling. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about <span className="font-medium">Node.js, and MongoDB</span>
       </p>
     </motion.section>
   );
