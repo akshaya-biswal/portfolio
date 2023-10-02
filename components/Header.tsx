2b1fbab34f2d636eff8ceead8362bacbce1034b8
akshaya.biswal <akshaya.join@google.com>
2023-10-02 12:00:28 +0530

    clsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { links } from "@/lib/data";
 import { motion } from "framer-motion";
+import clsx from "clsx";
 
 export default function Header() {
+  const [active, setActive] = useState("Home");
+
   return (
     <header className="z-[999] relative">
       <motion.div
@@ -23,10 +26,29 @@ export default function Header() {
                 animate={{ y: 0, opacity: 1 }}
               >
                 <Link
+                  className={clsx(
+                    "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition",
+                    {
+                      "text-gray-950": active === link.name,
+                    }
+                  )}
                   href={link.hash}
-                  className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition"
+                  onClick={() => {
+                    setActive(link.name);
+                  }}
                 >
                   {link.name}
+                  {link.name === active && (
+                    <motion.span
+                      className="bg-gray-100 rounded-full absolute inset-0 -z-10"
+                      layoutId="activeSection"
+                      transition={{
+                        type: "spring",
+                        stiffness: 380,
+                        damping: 30,
+                      }}
+                    ></motion.span>
+                  )}
                 </Link>
               </motion.li>
             ))}
