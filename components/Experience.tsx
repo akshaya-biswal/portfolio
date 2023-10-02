1026586646b9ef0150ecf031697b375d445b12de
akshaya.biswal <akshaya.join@google.com>
2023-10-02 17:27:25 +0530

    Experience not working

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -38,13 +38,41 @@ export default function Experience() {
                 fontSize: "1.5rem",
               }}
             >
-              <h3 className="">{item.title}</h3>
-              <p className="">{item.location}</p>
-              <p className="">{item.description}</p>
+              <h3 className="font-semibold capitalize">{item.title}</h3>
+              <p className="font-normal !mt-0">{item.location}</p>
+              <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
+                {item.description}
+              </p>
             </VerticalTimelineElement>
           </React.Fragment>
         ))}
       </VerticalTimeline>
     </section>
   );
+}
+
+{
+  /* <VerticalTimeline lineColor="">
+        <VerticalTimelineElement
+          contentStyle={{
+            background: "#f3f4f6",
+            boxShadow: "none",
+            border: "1px solid rgba(0, 0, 0, 0.05)",
+            textAlign: "left",
+            padding: "1.3rem 2rem",
+          }}
+          contentArrowStyle={{
+            borderRight: "0.4rem solid #9ca3af",
+          }}
+          date="1121"
+          icon="1212"
+          iconStyle={{
+            background: "white",
+            fontSize: "1.5rem",
+          }}
+        >
+          <h3 className="">32323</h3>
+          <p className="">location</p>
+          <p className="">description</p>
+        </VerticalTimelineElement> */
 }
