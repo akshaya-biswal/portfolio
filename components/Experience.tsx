6927fb4668544880a8444185688e755c49a68ea5
akshaya.biswal <akshaya.join@google.com>
2023-10-02 17:14:39 +0530

    Experience Not Working

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,48 +1,50 @@
 "use client";
 
-import useSectionInView from "@/lib/hooks";
 import React from "react";
+
 import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import SectionHeading from "./SectionHeading";
 import { experiencesData } from "@/lib/data";
+import useSectionInView from "@/lib/hooks";
+import SectionHeading from "./SectionHeading";
 
 export default function Experience() {
   const ref = useSectionInView("Experience");
 
   return (
-    <>
-      <section
-        className="scroll-mt-28 mb-28 sm:mb-40"
-        id="experience"
-        ref={ref}
-      >
-        <SectionHeading>My experience</SectionHeading>
-        <VerticalTimeline>
-          {experiencesData.map((experience, index) => (
-            <React.Fragment key={index}>
-              <VerticalTimelineElement
-                contentStyle={{
-                  background: "#f3f4f6",
-                  boxShadow: "none",
-                  border: "1px solid rgba(0, 0, 0, 0.05)",
-                  textAlign: "left",
-                  padding: "1.3rem 2rem",
-                }}
-              >
-                <h3 className="font-semibold capitalize">{experience.title}</h3>
-                <p className="font-normal !mt-0">{experience.location}</p>
-                <p className="!mt-1 !font-normal text-gray-700">
-                  {experience.description}
-                </p>
-              </VerticalTimelineElement>
-            </React.Fragment>
-          ))}
-        </VerticalTimeline>
-      </section>
-    </>
+    <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
+      <SectionHeading>My experience</SectionHeading>
+      <VerticalTimeline lineColor="">
+        {experiencesData.map((item, index) => (
+          <React.Fragment key={index}>
+            <VerticalTimelineElement
+              contentStyle={{
+                background: "#f3f4f6",
+                boxShadow: "none",
+                border: "1px solid rgba(0, 0, 0, 0.05)",
+                textAlign: "left",
+                padding: "1.3rem 2rem",
+              }}
+              contentArrowStyle={{
+                borderRight: "0.4rem solid #9ca3af",
+              }}
+              date={item.date}
+              icon={item.icon}
+              iconStyle={{
+                background: "white",
+                fontSize: "1.5rem",
+              }}
+            >
+              <h3 className="">{item.title}</h3>
+              <p className="">{item.location}</p>
+              <p className="">{item.description}</p>
+            </VerticalTimelineElement>
+          </React.Fragment>
+        ))}
+      </VerticalTimeline>
+    </section>
   );
 }
